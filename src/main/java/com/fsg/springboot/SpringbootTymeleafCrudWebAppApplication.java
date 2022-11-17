4b6ca0da1ce8b4b617e67d0a0c210f60f234294a
Ravikumarh6166 <ravikumar.h@4-serveinc.com>
2022-11-17 16:15:57 +0530

    Spring crud and pagination

---
+++ src/main/java/com/fsg/springboot/SpringbootTymeleafCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.fsg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTymeleafCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTymeleafCrudWebAppApplication.class, args);
+	}
+
+}
